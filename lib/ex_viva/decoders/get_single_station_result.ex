7cb2c37f2d9ab7fb9d127cd85bd845ac17b85998
Hannes Nevalainen <kwando@merciless.me>
2019-05-08 21:52:55 +0200

    Decompose to several decorder to functions.

--- lib/ex_viva/decoders/get_single_station_result.ex
+++ lib/ex_viva/decoders/get_single_station_result.ex
@@ -1,6 +1,15 @@
 defmodule ExViva.Decoders.GetSingleStationResult do
-  def handle_response({status, headers, %{"GetSingleStationResult" => %{"Felmeddelande" => nil, "ID" => station_id, "Samples" => samples}}}, _opts) do
-    {status, headers, %ExViva.StationSample{samples: map_samples(samples), station_id: station_id, requested_at: DateTime.utc_now()}}
+  def handle_response({status, headers, body}, _opts) do
+    {:ok, decoded_body} = decode(body)
+    {status, headers, decoded_body}
+  end
+
+  def decode(%{"GetSingleStationResult" => %{"Felmeddelande" => nil, "ID" => station_id, "Samples" => samples}}) do
+    {:ok, %ExViva.StationSample{samples: map_samples(samples), station_id: station_id, requested_at: DateTime.utc_now()}}
+  end
+
+  def decode(%{"GetSingleStationResult" => %{"Felmeddelande" => error}}) do
+    {:error, {:station_error, error}}
   end
 
   defp map_samples(samples) do
